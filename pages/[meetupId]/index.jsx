ef4f81f645e1284698e591185b566e7a2a322ba0
minggu99 <tuanphamtran99@gmail.com>
2022-03-22 15:52:29 +0700

    fixed fallback

--- pages/[meetupId]/index.jsx
+++ pages/[meetupId]/index.jsx
@@ -32,7 +32,7 @@ export const getStaticPaths = async () => {
   client.close();
 
   return {
-    fallback: false,
+    fallback: true,
     paths: meetups.map((meetup) => ({
       params: {
         meetupId: meetup._id.toString(),
